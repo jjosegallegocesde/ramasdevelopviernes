ebefafafd138a634f0de1ad8905e1e77df7e7609
209 <209@academicos.cesde.edu.co>
2022-09-30 19:03:23 -0500

    NTV9 se crea fumcion lambda

---
+++ nuevas/anamonsalve.py
@@ -0,0 +1,14 @@
+def sumar_num (numero1, numero2):
+   #declarando la función
+    return numero1,numero2
+
+resultado=sumar_num(1,2) # invocando la funcion    
+print(resultado) 
+
+#funciones lambda
+#simplemente otra forma de escribir una funcion 
+
+sumar_numeros = lambda numero1,numero2: numero1+numero2
+
+resultado=sumar_numeros(1,2) # invocando la funcion
+print(resultado )
