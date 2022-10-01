de35ecd716538c052d3a2d86fde99f95678fbed1
209 <209@academicos.cesde.edu.co>
2022-09-30 19:02:59 -0500

    NTV9 se crea funcion

---
+++ nuevas/bryanlondono.py
@@ -0,0 +1,12 @@
+def sumar(numero1,numero2): #Declarando la funcion
+    return numero1+numero2
+
+resultado=sumar(1,1) #Invocando la funcion
+print(resultado)
+
+#Funciones lambda
+#Simplemente otra forma de escribir otra funcion
+SumarNumeros = lambda numero1,numero2:numero1+numero2
+
+resultado=SumarNumeros(1,2) #Invocando la funcion
+print(resultado)
