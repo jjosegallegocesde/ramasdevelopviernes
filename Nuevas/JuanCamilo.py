3c1288d7bc1b214f680e442176ad1bc090b87e0e
209 <209@academicos.cesde.edu.co>
2022-09-30 19:02:58 -0500

    NTV9 Se crea función

---
+++ Nuevas/JuanCamilo.py
@@ -0,0 +1,11 @@
+'''def sumar(numero1, numero2):
+    return numero1 + numero2
+
+total = sumar(8, 8)'''
+
+
+sumar = lambda numero1, numero2: (numero1 + numero2)
+
+total = sumar(8, 9)
+
+print(total)
