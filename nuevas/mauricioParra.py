21ff5878ebb8f27abfabb4fbf5d63dc0638b0ff0
209 <209@academicos.cesde.edu.co>
2022-09-30 19:03:01 -0500

    NTV9 Se crea funcion lambda

---
+++ nuevas/mauricioParra.py
@@ -0,0 +1,15 @@
+def sumar(numero1,numero2): #Declarando la funcion
+    return numero1 + numero2
+
+suma = sumar(2,3)#invocando la funcion
+print(suma)
+
+
+
+#funciones lambda
+#simplemente otra forma de escribir una funcion
+
+sumarNumeros = lambda numero3,numero4:numero3+numero4
+
+suma = sumarNumeros(2,3)#invocando la funcion
+print(suma)
