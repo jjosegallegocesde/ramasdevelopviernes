58b1b2b529d0a0ba8e56dca6d9230db757176b40
Pablo <Pablo06lara87@gmail.com>
2022-09-30 19:07:21 -0500

    Se crea funcion lambda

--- nuevas/pabloLara.py
+++ nuevas/pabloLara.py
@@ -3,7 +3,16 @@ def sumar(numeroUno, numeroDos):
     return numeroUno + numeroDos
 
 
-resultado = sumar(10,10)
+""" resultado = sumar(10,10)
 
 print(resultado)
+ """
+#Funciones lambda
+#Simplemente es otra forma de escribir una funcion 
+
+sumarNumeros = lambda numeroUno, numeroDos: numeroUno + numeroDos
+
+resultado = sumarNumeros(10,10)
+print(resultado)
+
 
