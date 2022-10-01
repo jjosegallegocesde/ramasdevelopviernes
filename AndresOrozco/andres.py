4f3d4583058bd009a805c4d282ed14482f9b615b
209 <209@academicos.cesde.edu.co>
2022-09-30 19:07:51 -0500

    NTV9 funcion lambda

---
+++ AndresOrozco/andres.py
@@ -0,0 +1,14 @@
+#Declarando la funcion suma
+def sumar(numero1,numero2):
+    suma= numero1+numero2
+    return print(suma)
+
+#invocando la funcion
+sumar(6,7)
+
+sumarnumeros = lambda numero1,numero2:numero1+numero2
+resultado = sumarnumeros(1,1)
+print(resultado)
+
+
+
