9a8c9393fa568a122b2b71b30b8da78990180490
Miguelrpo09 <72237259+miguelrpo09@users.noreply.github.com>
2022-09-30 19:07:04 -0500

    NTV9 Se crea funcion lambda

---
+++ nuevas/miguelRestrepo.py
@@ -0,0 +1,17 @@
+""" Declarando la funcion """
+def sumar(numero1, numero2): 
+    return numero1 + numero2
+
+""" invocando la funcion """
+resultado=sumar(1,1)
+
+print(resultado)
+
+#funciones lambda
+#Simplemente otra forma de escribir una funcion
+#la funcion lambda solo se escribe en una linea
+
+sumarNumeros= lambda numero1, numero2: numero1+numero2
+resultado=sumarNumeros(1,1)
+
+print(resultado)
