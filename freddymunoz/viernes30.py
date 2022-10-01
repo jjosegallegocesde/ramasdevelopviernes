604653c1e5f0a7e96fc0e25c2b5b5fb11caae23a
209 <209@academicos.cesde.edu.co>
2022-09-30 19:08:20 -0500

    funcion saludar

---
+++ freddymunoz/viernes30.py
@@ -0,0 +1,14 @@
+
+#declarar la función
+def saludar():
+    return print('hola')
+
+#invocar la función
+saludar()
+
+
+#declarar la función lambda λ
+saluda = lambda : print ("hola")
+
+#invocar la función λ
+saluda()
