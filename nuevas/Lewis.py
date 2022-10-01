52563933fddbd7dc98dafb81ee4bad568c2144c6
suarez <suarezlewis97@gmai.com>
2022-09-30 19:07:50 -0500

    se crea funcion

---
+++ nuevas/Lewis.py
@@ -0,0 +1,15 @@
+
+def sumar(numero1,nuemro2): #declarando la función
+    return numero1+nuemro2
+
+resultado=sumar(1,1)#invocando la funcion
+print(resultado)
+
+#funciones lambda
+#simplemente otra forma de escribir una funcion
+
+sumar_numeros= lambda numero1,numero2:numero1+numero2
+resultado=sumar_numeros(1,2)#invocando la funcion
+print(resultado)
+
+    
